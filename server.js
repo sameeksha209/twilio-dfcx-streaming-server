e5415c85af13229257bb5ecd97ec0dc6d636c9c8
sameeksha.chourasiya <sameeksha.chourasiya@accenture.com>
2025-12-31 15:09:04 +0530

    setting up build

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require("express");
+const app = express();
+
+const mediasocket = require('./websocket/mediasocket');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Logging middleware
+app.use((req, res, next) => {
+  console.log("Incoming Request:", req.method, req.url);
+  next();
+});
+
+// Routes
+app.use("/", mediasocket);
+
+// Start server
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening on port ${port}`);
+});
